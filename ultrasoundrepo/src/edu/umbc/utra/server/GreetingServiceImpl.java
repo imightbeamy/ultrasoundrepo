f85eabdc9e78e1627520bb5d0ed817c2ef30356e
coyote.j.p.m <coyote.j.p.m@gmail.com>
2011-10-27 20:04:11 -0400

    Formatted main default class. Added test directory.

--- ultrasoundrepo/src/edu/umbc/utra/server/GreetingServiceImpl.java
+++ ultrasoundrepo/src/edu/umbc/utra/server/GreetingServiceImpl.java
@@ -7,17 +7,16 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 /**
  * The server side implementation of the RPC service.
  */
-@SuppressWarnings("serial")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-		GreetingService {
-
-	public String greetServer(String input) throws IllegalArgumentException {
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService
+{
+	public String greetServer(String input) throws IllegalArgumentException
+	{
 		// Verify that the input is valid. 
-		if (!FieldVerifier.isValidName(input)) {
+		if (!FieldVerifier.isValidName(input))
+		{
 			// If the input is not valid, throw an IllegalArgumentException back to
 			// the client.
-			throw new IllegalArgumentException(
-					"Name must be at least 4 characters long");
+			throw new IllegalArgumentException("Name must be at least 4 characters long");
 		}
 
 		String serverInfo = getServletContext().getServerInfo();
@@ -27,8 +26,7 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
 
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>" + userAgent;
 	}
 
 	/**
@@ -38,11 +36,12 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 	 * @param html the html string to escape
 	 * @return the escaped string
 	 */
-	private String escapeHtml(String html) {
-		if (html == null) {
+	private String escapeHtml(String html)
+	{
+		if (html == null)
+		{
 			return null;
 		}
-		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;");
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 	}
 }
