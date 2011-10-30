2c3cdeb4c9bee7d4ffa4fe0fd302d62d1e4485f9
coyote.j.p.m <coyote.j.p.m@gmail.com>
2011-10-30 15:55:56 -0400

    Did initial coding on base data classes.

--- UltrasoundRepo2.0/src/edu/umbc/ultra/logic/Patient.java
+++ UltrasoundRepo2.0/src/edu/umbc/ultra/logic/Patient.java
@@ -1,6 +1,49 @@
 package edu.umbc.ultra.logic;
 
+import java.util.Date;
+
 public class Patient
 {
 	public static enum Gender {MALE, FEMALE}
+	
+	private String firstName;
+	private String lastName;
+	private Date dob;
+	private Gender gender;
+	private String id;
+	
+	
+	
+	public Patient(String firstName, String lastName, Date dob, Gender gender, String id)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.gender = gender;
+		this.id = id;
+	}
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public String getLastName()
+	{
+		return lastName;
+	}
+	public Date getDob()
+	{
+		return dob;
+	}
+	public Gender getGender()
+	{
+		return gender;
+	}
+	public String getId()
+	{
+		return id;
+	}
 }
