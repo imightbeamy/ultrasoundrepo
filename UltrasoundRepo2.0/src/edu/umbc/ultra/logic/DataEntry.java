e58333b4d50d1dfd7d6efbfa03ac12c74e69679a
coyote.j.p.m <coyote.j.p.m@gmail.com>
2011-10-30 17:53:01 -0400

    Finished the DataEntry class stub, does not include video functionality. Started work on the UploadController, again, does not include videos.

--- UltrasoundRepo2.0/src/edu/umbc/ultra/logic/DataEntry.java
+++ UltrasoundRepo2.0/src/edu/umbc/ultra/logic/DataEntry.java
@@ -9,4 +9,21 @@ public class DataEntry
 	private Date timestamp;
 	private ArrayList<Comment> comments;
 	private Patient patient;
+	
+	public User getAuthor()
+	{
+		return author;
+	}
+	public Date getTimestamp()
+	{
+		return timestamp;
+	}
+	public ArrayList<Comment> getComments()
+	{
+		return comments;
+	}
+	public Patient getPatient()
+	{
+		return patient;
+	}
 }
