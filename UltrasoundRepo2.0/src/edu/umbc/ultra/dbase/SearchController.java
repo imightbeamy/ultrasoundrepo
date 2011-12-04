1b3ef45b6e1c33331d3091073209a2ef7f03883f
amy <amy@gmail.com>
2011-12-04 02:42:59 -0500

    Keyword search done. Still working on advanced search but almost done.

--- UltrasoundRepo2.0/src/edu/umbc/ultra/dbase/SearchController.java
+++ UltrasoundRepo2.0/src/edu/umbc/ultra/dbase/SearchController.java
@@ -1,6 +1,7 @@
 package edu.umbc.ultra.dbase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
@@ -111,12 +112,16 @@ public class SearchController {
 	}
 
 	public ArrayList<DataEntry> searchByKeyword(ArrayList<String> keywords) {
+		return KeysToDataEntries(searchByKeywordforKeys(keywords));
+	}
+	
+	public ArrayList<Key> searchByKeywordforKeys(ArrayList<String> keywords) {
 		ArrayList<Key> dataEntries = searchComments(keywords, null);
 		dataEntries.addAll(searchPatients(keywords));
 		for(String word: keywords) {
 		dataEntries.addAll(searchByUser(word));
 		}
-		return KeysToDataEntries(dataEntries);
+		return dataEntries;
 	}
 	
 	public ArrayList<DataEntry> KeysToDataEntries(ArrayList<Key> dataEntities) {
@@ -191,23 +196,26 @@ public class SearchController {
 		// Get an instance of the data store controller
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 		ArrayList<Key> dataEntryKeys = new ArrayList<Key>();
-		
 		ArrayList<Query> querys = new ArrayList<Query>();
-		Query first_query = new Query("Patient");
-		first_query.addFilter("FirstName", Query.FilterOperator.EQUAL, first);
-		first_query.setKeysOnly();
-		querys.add(first_query);
 		
-		Query last_query = new Query("Patient");
-		last_query.addFilter("LastName", Query.FilterOperator.EQUAL, last);
-		last_query.setKeysOnly();
-		querys.add(last_query);
-
-		Query g_query = new Query("Patient");
-		g_query.addFilter("g_query", Query.FilterOperator.EQUAL, gender.toString());
-		g_query.setKeysOnly();
-		querys.add(g_query);
-		
+		if(first != null) {
+			Query first_query = new Query("Patient");
+			first_query.addFilter("FirstName", Query.FilterOperator.EQUAL, first);
+			first_query.setKeysOnly();
+			querys.add(first_query);
+		}
+		if(first != null) {
+			Query last_query = new Query("Patient");
+			last_query.addFilter("LastName", Query.FilterOperator.EQUAL, last);
+			last_query.setKeysOnly();
+			querys.add(last_query);
+		}
+		if(first != null) {
+			Query g_query = new Query("Patient");
+			g_query.addFilter("g_query", Query.FilterOperator.EQUAL, gender.toString());
+			g_query.setKeysOnly();
+			querys.add(g_query);
+		}
 		for(Query q: querys) {
 			PreparedQuery pq = datastore.prepare(q);
 			for (Entity result : pq.asIterable()) {
@@ -272,150 +280,17 @@ public class SearchController {
 	public ArrayList<DataEntry> searchForEntries(String firstName,
 			String lastName, Gender gender, String chiefComplaint,
 			String keywords, String userEmail) {
+		ArrayList<Key> results = new ArrayList<Key>();
+		if(firstName != null || lastName!=null || gender != null) {
+			results.addAll(searchPatientFields(firstName, lastName, gender, null));
+		}
+		if(userEmail != null) {
+			results.addAll(searchByUser(userEmail));
+		}
 		
-		if (firstName != null || lastName != null || gender != null) {
-			return searchForPatients(firstName, lastName, gender,
-					chiefComplaint, keywords, userEmail);
-		}
-
-		return searchDataEntriesByPatientKey(
-				null,
-				chiefComplaint,
-				keywords,
-				(userEmail == null || userEmail == "") ? null : KeyFactory
-						.createKey("User", userEmail));
-	}
-
-	private ArrayList<DataEntry> searchForPatients(String firstName,
-			String lastName, Gender gender, String chiefComplaint,
-			String keyWords, String userEmail) {
-		ArrayList<DataEntry> results = new ArrayList<DataEntry>();
-		
-		// Build a query searching on Patient entities
-		Query query = new Query("Patient");
-
-		// Add filter by user
-		if (userEmail != null && userEmail != "") {
-			// If a user email is specified, add an ancestor path based on that user's email address
-			query.setAncestor(KeyFactory.createKey("User", userEmail));
-		}
-
-		// Add filters by patient
-		if (firstName != null && firstName != "") {
-			// If a first name is specified, add a filter for the first name property
-			query.addFilter("FirstName", FilterOperator.EQUAL, firstName);
-		}
-		if (lastName != null && lastName != "") {
-			// If a last name is specified, add a filter for the last name property
-			query.addFilter("LastName", FilterOperator.EQUAL, lastName);
-		}
-		if (gender != null) {
-			// If a gender is specified, add a filter for the gender property
-			query.addFilter("Gender", FilterOperator.EQUAL,
-					Patient.getGenderAsString(gender));
-		}
-
-		// Get an instance of the data store controller
-		DatastoreService datastore = DatastoreServiceFactory
-				.getDatastoreService();
-		
-		// Submit the previously prepared query
-		PreparedQuery pq = datastore.prepare(query);
-
-		// Iterate over the results of the query
-		for (Entity patientEntity : pq.asIterable()) {
-			ArrayList<DataEntry> entries = searchDataEntriesByPatientKey(
-					patientEntity.getKey(), chiefComplaint, keyWords,
-					KeyFactory.createKey("User", userEmail));
-			results.addAll(entries);
-		}
-
-		return results;
-	}
-
-	private ArrayList<DataEntry> searchDataEntriesByPatientKey(Key patientKey,
-			String chiefComplaint, String keywords, Key userKey) {
-		// Do two queries, one for chief complaint and one for the other
-		// keywords
-		DatastoreService datastore = DatastoreServiceFactory
-				.getDatastoreService();
-
-		ArrayList<DataEntry> results = new ArrayList<DataEntry>();
-		ArrayList<Key> dataEntryKeys = new ArrayList<Key>();
-
-		if (chiefComplaint != null && chiefComplaint != "") {
-			Query query = new Query("Keyword");
-			if (patientKey != null) {
-				query.setAncestor(patientKey);
-			} else if (userKey != null) {
-				query.setAncestor(userKey);
-			}
-			query.addFilter("Type", FilterOperator.EQUAL, "CC");
-			query.addFilter("Word", FilterOperator.EQUAL,
-					chiefComplaint.toLowerCase());
-
-			PreparedQuery pq = datastore.prepare(query);
-			for (Entity kwe : pq.asIterable()) {
-				Key dataEntityKey = kwe.getParent().getParent();
-				if (!dataEntryKeys.contains(dataEntityKey)) {
-					dataEntryKeys.add(dataEntityKey);
-				}
-			}
-		}
-
-		if (keywords != null && keywords != "") {
-			Query query = new Query("Keyword");
-			if (patientKey != null) {
-				query.setAncestor(patientKey);
-			} else if (userKey != null) {
-				query.setAncestor(userKey);
-			}
-			query.addFilter("Type", FilterOperator.EQUAL, "KW");
-			ArrayList<String> kwc = new ArrayList<String>();
-			System.out.println("Splitting keywords...");
-			for (String kw : keywords.toLowerCase().split("\\s+")) {
-				kwc.add(kw);
-				System.out.println("\"" + kw + "\"");
-			}
-			query.addFilter("Word", FilterOperator.IN, kwc);
-
-			PreparedQuery pq = datastore.prepare(query);
-			int count = 0;
-			for (Entity kwe : pq.asIterable()) {
-				System.out.println("Processing kwr " + (++count));
-				Key dataEntityKey = kwe.getParent().getParent();
-				if (!dataEntryKeys.contains(dataEntityKey)) {
-					dataEntryKeys.add(dataEntityKey);
-				}
-			}
-		}
-
-		for (Key key : dataEntryKeys) {
-			try {
-				Entity dataEntity = datastore.get(key);
-				DataEntry de = getDataEntryFromEntity(dataEntity);
-				results.add(de);
-			} catch (EntityNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return results;
-	}
-
-	// Gender being null means search for any gender
-	public ArrayList<DataEntry> searchForEntriesTEST(String firstName,
-			String lastName, Gender gender, String chiefComplaint,
-			String keywords, String userEmail) {
-		ArrayList<DataEntry> results = new ArrayList<DataEntry>();
-		ArrayList<Comment> comments = new ArrayList<Comment>();
-		User user = new User();
-		// Patient newPatient = new Patient("Nathan", "Broemm", new Date(),
-		// Gender.MALE, "nab0415");
-		Comment testComment = new Comment("Turn the Computer off and on!", user, "lol");
-		comments.add(testComment);
-		// results.add(new DataEntry(comments, newPatient, new User()));
-		return results;
+		results.addAll(searchByKeywordforKeys(new ArrayList<String>(Arrays.asList(keywords.split(" ")))));
+		results.addAll(searchComments(new ArrayList<String>(Arrays.asList(chiefComplaint.split(" "))), "CC"));
+		return KeysToDataEntries(results);
 	}
 
 	/* Shameful shameful singleton code */
